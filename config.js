fdd92cf4b2b98446079f5538d9b9dc94ba350b90
feliciosan <feliciosantoslive@outlook.com>
2021-01-02 22:10:40 -0300

    App port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     api: {
-        port: process.env.API_PORT,
+        port: process.env.PORT || process.env.API_PORT,
         host: process.env.API_HOST,
         secret_key: process.env.API_SECRET_KEY,
     },
