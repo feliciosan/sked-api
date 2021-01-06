e7a6658b7eec630d73a05df965c89e8b7dd76919
feliciosan <feliciosantoslive@outlook.com>
2021-01-05 23:26:52 -0300

    Brings account attributes

--- src/services/account.js
+++ src/services/account.js
@@ -2,7 +2,7 @@ const AccountDao = require('../dao/account');
 
 const find = async ({ filter }) => {
     const account = await AccountDao.find(filter, {
-		attributes: ['id', 'name'],
+		attributes: ['id', 'name', 'address', 'telephone'],
 	});
 
     return { account };
